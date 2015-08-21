c63fa347880b368d40dc8e27c8918ab19ba9ec2b
deluxturtle <sebaturtle@gmail.com>
2015-08-21 18:39:49 -0500

    Update
    Got all the way to Extensions in the doc

--- Lab01_ASeba/Assets/Editor/ScriptEditorOrganizer.cs
+++ Lab01_ASeba/Assets/Editor/ScriptEditorOrganizer.cs
@@ -9,19 +9,60 @@ public class ScriptEditorOrganizer : MonoBehaviour {
     [MenuItem("Tool Creation/Create folder")]
     public static void Createfolder()
     {
+        //Create All the project folders.
+        //Assets/Dynamic Assets
         AssetDatabase.CreateFolder("Assets", "Dynamic Assets");
+
+        //Assets/Dynamic Assets/Resources
         AssetDatabase.CreateFolder("Assets/Dynamic Assets", "Resources");
-        string dyAssetsResources = "Assets/Dynamic Assets/Resources";
-        AssetDatabase.CreateFolder("Assets", "Materials");
-        System.IO.File.WriteAllText(Application.dataPath + "/Materials/folderStructure.txt", "This Folder is for storing materials!");
-        AssetDatabase.CreateFolder("Assets", "Textures");
-        System.IO.File.WriteAllText(Application.dataPath + "/Textures/folderStructure.txt", "This Folder is for storing textures!");
-        AssetDatabase.CreateFolder("Assets", "Prefabs");
-        System.IO.File.WriteAllText(Application.dataPath + "/Prefabs/folderStructure.txt", "This Folder is for storing Prefabs!");
-        AssetDatabase.CreateFolder("Assets", "Scripts");
-        AssetDatabase.CreateFolder("Assets", "Scenes");
-        AssetDatabase.CreateFolder("Assets", "Animations");
-        AssetDatabase.CreateFolder("Assets/Animations", "AnimationControllers");
+
+        //Assets/Dynamic Assets/Resources/Animations
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Animations");
+        //Assets/Dynamic Assets/Resources/Animations/Sources
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Animations", "Sources");
+
+        //Assets/Dynamic Assets/Resources/Animation Controllers
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Animation Controllers");
+
+        //Assets/Dynamic Assets/Resources/Effects
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Effects");
+        
+        //Assets/Dynamic Assets/Resources/Models
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Models");
+        //Assets/Dynamic Assets/Resources/Models/Characters
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Models", "Characters");
+        //Assets/Dynamic Assets/Resources/Models/Environment
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Models", "Environment");
+
+        //Assets/Dynamic Assets/Resources/Prefabs
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Prefabs");
+        //Assets/Dynamic Assets/Resources/Prefabs/Common
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Prefabs", "Common");
+
+        //Assets/Dynamic Assets/Resources/Sounds
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Sounds");
+        //Assets/Dynamic Assets/Resources/Sounds/Music
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Sounds", "Music");
+        //Assets/Dynamic Assets/Resources/Sounds/Music/Common
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Sounds/Music", "Common");
+        //Assets/Dynamic Assets/Resources/Sounds/SFX
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Sounds", "SFX");
+        //Assets/Dynamic Assets/Resources/Sounds/SFX/Common
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources/Sounds/SFX", "Common");
+        //Assets/Dynamic Assets/Resources/Sounds
+        AssetDatabase.CreateFolder("Assets/Dynamic Assets/Resources", "Textures");
+        //Assets/Dynamic Assets/Resources/folderStructure.txt
+        System.IO.File.WriteAllText(Application.dataPath + "Assets/Dynamic Assets/Resources", "Place all your dynamic resources in this folder.");
+
+        //Assets/Editor
+        AssetDatabase.CreateFolder("Assets", "Editor");
+        //Assets/Editor/folderSctructure.txt
+        System.IO.File.WriteAllText(Application.dataPath + "Assets/Editor", "Place editor scripts in this folder.");
+
+        //Assets/Extensions
+        AssetDatabase.CreateFolder("Assets", "Extensions");
+        //Assets/Extensions/folderStructure.txt
+        System.IO.File.WriteAllText(Application.dataPath + "Assets/Extensions", "Place extensions in this folder.");
 
         AssetDatabase.Refresh();
     }
